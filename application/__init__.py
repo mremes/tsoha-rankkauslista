5f6eda9c5c9ba687437f39808373905cae7b3d7a
Matti Remes <remes@iki.fi>
2018-10-14 22:06:35 +0300

    Edit rankings views to conform to the new authorization mechanism

--- application/__init__.py
+++ application/__init__.py
@@ -1,15 +1,13 @@
 import os
 
 from flask import Flask
+from flask_login import current_user
 from flask_sqlalchemy import SQLAlchemy
-from application.extensions import csrf
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.urandom(32)
 app.config['WTF_CSRF_SECRET_KEY'] = os.urandom(32)
-
-csrf.init_app(app)
 
 if os.environ.get('HEROKU'):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
@@ -19,12 +17,49 @@ else:
 
 db = SQLAlchemy(app)
 
+# roles in login_required
+from functools import wraps
+
+
+def login_required(roles=None):
+    if roles is None:
+        roles = ["ANY"]
+
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user:
+                return login_manager.unauthorized()
+
+            if not current_user.is_authenticated:
+                return login_manager.unauthorized()
+
+            unauthorized = False
+
+            if "ANY" not in roles:
+                unauthorized = True
+
+                for user_role in current_user.roles():
+                    if user_role in roles:
+                        unauthorized = False
+                        break
+
+            if unauthorized:
+                return login_manager.unauthorized()
+
+            return fn(*args, **kwargs)
+
+        return decorated_view
+
+    return wrapper
+
+
 from application import views
 
 from application.rankings import models
 from application.rankings import views
 
-from application.auth import models
+from application.auth.models import user
 from application.auth import views
 
 # noinspection PyBroadException
@@ -34,5 +69,8 @@ except Exception as ex:
     pass
 
 from application.auth.login import login_manager
-
 login_manager.init_app(app)
+
+from application.extensions import csrf
+
+csrf.init_app(app)
